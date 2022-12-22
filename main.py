24e1931bf503bd2467f8b1b58c6ad6fd2f470985
Michael <micheaol80@gmail.com>
2022-12-22 15:53:35 +0100

    Add multiple line of strings

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-number = 0
 
-if number > 0:
-    print(f"Number {number} is positive")
 
-elif number == 0:
-    print(f"number is {number}")
-else:
-    print(f"Number {number} is negative")
+# if statement:
+# number = 0
+#
+# if number > 0:
+#     print(f"Number {number} is positive")
+#
+# elif number == 0:
+#     print(f"number is {number}")
+# else:
+#     print(f"Number {number} is negative")
+
+
+
+
+# Multiple line of string
+
+name = "Michael"
+email = f"""
+Hello {name},
+
+My name is {name}, I am writting to inform you of my promotion.
+
+Thank you for your help.
+
+Kinds,
+{name}
+"""
+
+print(email)
+
+
+
+
+
+
